d660abc9dbff04df371341be9bc8358852349c7b
nima isselee <nima.isselee@protonmail.com>
2025-05-18 17:55:24 +0100

    feat: Read user agent header.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
-use std::thread;
 use std::io::{BufRead, BufReader, IoSlice, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::path;
+use std::thread;
 
-use default::default;
 use anyhow::{anyhow, Result};
+use default::default;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -18,11 +18,8 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                //println!("accepted new connection");
-                thread::spawn(|| {
-                    handle_connection(stream)
-                });
-                //println!("Returned 200 OK to {}", stream.peer_addr());
+                println!("accepted new connection: {:?}", stream.peer_addr());
+                thread::spawn(|| handle_connection(stream));
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -42,31 +39,35 @@ impl HttpMethod {
         match method {
             "GET" => Ok(Self::Get),
             "POST" => Ok(Self::Post),
-            _ => Err(anyhow!("Could not parse {} into HttpMethod", method))
+            _ => Err(anyhow!("Could not parse {} into HttpMethod", method)),
         }
     }
-
 }
 
 #[derive(Debug)]
-struct Request<'a> {
+struct Request {
     pub method: HttpMethod,
-    pub path: &'a str,
-    pub http_version: &'a str,
-    // Host: localhost
-    pub headers: HashMap<&'a str, &'a str>,
-    pub body: Option<&'a str>,
+    pub path: String,
+    pub http_version: String,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
 }
 
 fn valid_path(path: &str) -> bool {
     true
 }
 
-impl<'a> Request<'a> {
-    fn parse(request_line: &'a str) -> Result<Self> {
+impl Request {
+    fn from_stream(stream: &TcpStream) -> Result<Self> {
+        let reader = BufReader::new(stream);
+        let mut reader_lines = reader.lines();
+        let request_line = reader_lines.next().unwrap().unwrap();
+
         let mut request_split = request_line.split_whitespace();
         let method = {
-            let method_str = request_split.next().ok_or(anyhow!("Couldn't parse request method. No data to parse."))?;
+            let method_str = request_split
+                .next()
+                .ok_or(anyhow!("Couldn't parse request method. No data to parse."))?;
             HttpMethod::parse(&method_str)?
         };
 
@@ -75,9 +76,9 @@ impl<'a> Request<'a> {
                 anyhow!("Couldn't get path from request. Http requests should be space separated, e.g. `<method> <path> <http_version>`, but no space was found."
             ))?;
             if !valid_path(path_str) {
-                return Err(anyhow!("Path {} is not valid", path_str))
+                return Err(anyhow!("Path {} is not valid", path_str));
             };
-            path_str
+            path_str.to_string()
         };
 
         let http_version = {
@@ -85,16 +86,31 @@ impl<'a> Request<'a> {
                 anyhow!("Couldn't get http version from request. Http requests should be space separated, e.g. `<method> <path> <http_version>`, but there was no 3rd element when space separating."
             ))?;
             match version {
-                "HTTP/1.1" => version,
-                _ => return Err(anyhow!("Bad HTTP version: {}", version))
+                "HTTP/1.1" => version.to_string(),
+                _ => return Err(anyhow!("Bad HTTP version: {}", version)),
             }
         };
+        
+        let mut headers: HashMap<String, String> = HashMap::new();
+
+        // Keep reading until we get an empty line
+        let _ = reader_lines
+            .take_while(|x| match x {
+                Ok(v) => v != "",
+                _ => false,
+            })
+            .map(|x| {
+                let inner: String = x.ok().unwrap();
+                let header_split = inner.split_once(": ").unwrap();
+                headers.insert(header_split.0.to_string(), header_split.1.to_string());
+            })
+            .collect::<Vec<_>>();
 
         Ok(Self {
             method,
             path,
             http_version,
-            headers: HashMap::new(),
+            headers,
             body: None,
         })
     }
@@ -137,14 +153,12 @@ impl Default for Response {
 }
 
 impl Response {
-
     fn write_to_stream(self, mut stream: &TcpStream) -> Result<usize> {
-
         let mut stream_output = Vec::from([
             IoSlice::new(b"HTTP/1.1 "),
             IoSlice::new(self.http_code.to_tcp_format().as_bytes()),
-            IoSlice::new(b"\r\n")]
-        );
+            IoSlice::new(b"\r\n"),
+        ]);
 
         for (k, v) in self.headers.iter() {
             stream_output.push(IoSlice::new(k.as_bytes()));
@@ -155,7 +169,9 @@ impl Response {
 
         stream_output.push(IoSlice::new(b"\r\n"));
 
-        self.content.as_ref().map(|s| stream_output.push(IoSlice::new(s.as_bytes())));
+        self.content
+            .as_ref()
+            .map(|s| stream_output.push(IoSlice::new(s.as_bytes())));
 
         let write_result = stream.write_vectored(&stream_output);
         match write_result {
@@ -163,24 +179,18 @@ impl Response {
                 println!("Sent {n} bytes back.");
                 Ok(n)
             }
-            Err(err) => Err(anyhow!("Could not write response to stream: {}", err))
+            Err(err) => Err(anyhow!("Could not write response to stream: {}", err)),
         }
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let reader = BufReader::new(&stream);
+    let request = Request::from_stream(&stream).unwrap();
 
-    // We should never be getting empting requests (at the moment at least..)
-    // TODO: Probably shouldn't crash on empty lines.
-    //   Enough concurrent requests is making this catch empty lines.
-    let raw_stream = reader.lines().next().unwrap().unwrap();
-    let request = Request::parse(&raw_stream).unwrap();
-
-    match request.path {
+    match request.path.as_str() {
         "/" => {
             Response::default().write_to_stream(&stream).unwrap();
-        }
+        },
         path => {
             if path.starts_with("/echo") {
                 let echo_word = path[1..].split_once('/').unwrap().1;
@@ -192,76 +202,30 @@ fn handle_connection(mut stream: TcpStream) {
                 let response = Response {
                     http_code: HttpCode::Ok,
                     headers,
-                    content: Some(echo_word.to_owned())
+                    content: Some(echo_word.to_owned()),
+                };
+
+                response.write_to_stream(&stream).unwrap();
+            } else if path.starts_with("/user-agent") {
+
+                let mut headers: HashMap<String, String> = HashMap::new();
+
+                headers.insert("Content-Type".into(), "text/plain".into());
+                headers.insert("Content-Length".into(), format!("{}", request.headers.get("User-Agent").unwrap().len()));
+
+                let response = Response {
+                    http_code: HttpCode::Ok,
+                    headers,
+                    content: Some(request.headers.get("User-Agent").unwrap().to_owned()),
                 };
 
                 response.write_to_stream(&stream).unwrap();
 
             } else {
-                stream.write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes()).unwrap();
+                stream
+                    .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                    .unwrap();
             }
         }
-
     }
-
-}
-
-
-#[allow(non_snake_case)]
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse_request__basic_get_requests__ok() {
-        let mut test_request: &str = "GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n";
-
-        let request = Request::parse(test_request).unwrap();
-
-        assert_eq!(request.path, "/index.html");
-        assert_eq!(request.method, HttpMethod::Get);
-        assert_eq!(request.http_version, "HTTP/1.1");
-
-        test_request = "GET / HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n";
-        let request = Request::parse(test_request).unwrap();
-
-        assert_eq!(request.path, "/");
-        assert_eq!(request.method, HttpMethod::Get);
-        assert_eq!(request.http_version, "HTTP/1.1");
-
-    }
-
-    #[test]
-    fn test_parse_request__bad_method__fail() {
-        let test_request: &str = "POTATO /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n";
-        assert!(Request::parse(test_request).is_err());
-    }
-
-    #[test]
-    fn test_parse_request__missing_values__fail() {
-        let mut test_request: &str = "GET /index.html ";
-        assert!(Request::parse(test_request).is_err());
-
-        test_request = "GET /index.html";
-        assert!(Request::parse(test_request).is_err());
-
-        test_request = " /index.html";
-        assert!(Request::parse(test_request).is_err());
-
-        test_request = " /index.html ";
-        assert!(Request::parse(test_request).is_err());
-
-        test_request = "GET ";
-        assert!(Request::parse(test_request).is_err());
-
-    }
-
-
-
-    // #[test]
-    // fn test_parse_method() {
-    //     let method = parse_method(&TEST_REQUEST_LINE.to_string()).unwrap();
-    //     assert_eq!(method, "GET");
-    // }
-
 }
